9910b47b8428384630def37d61d00c798a10c223
Alex Wallar <aw204@st-andrews.ac.uk>
2015-09-17 12:11:13 -0400

    Added docs for the nameserver

--- src/jammi/nameserver.py
+++ src/jammi/nameserver.py
@@ -7,8 +7,29 @@ import threading
 
 
 class NameServer(object):
+    """
+    A name server class that is used to aggregate masters that are
+    sending heartbeat messages over the network. This class is used
+    for the discovery of other masters
+    """
 
     def __init__(self, host, port, interval):
+        """
+        Initializes a NameServer
+
+        Parameters
+        ----------
+        host: string
+            The host of the multicast group
+
+        port: integer
+            The port of the multicast group
+
+        interval: number
+            The amount of time that can elapse between heartbeat messages
+            before the remote master is labeled "dead"
+        """
+
         self.host = host
         self.port = port
         self.interval = interval
@@ -18,17 +39,82 @@ class NameServer(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((host, port))
 
-    def kill():
+    def kill(self):
+        """
+        Kills the name server thread
+        """
+
         self.running = False
         return self
 
-    def start():
+    def get_alive(self):
+        """
+        Returns the names of masters that are still labeled as "alive"
+
+        Returns
+        -------
+        alive: list of strings
+            A list of master names that are still alive
+        """
+
+        alive = list()
+        ctime = time.time()
+        for name, t in times.iteritems():
+            if ctime - t < interval:
+                alive.append(name)
+        return alive
+
+    def get_data(self, name):
+        """
+        Gets the data associated with a given master name
+
+        Parameters
+        ----------
+        name: string
+            The name of the master
+
+        Returns
+        -------
+        A: dict
+            The associated data of the master
+        """
+
+        return self.masters[name]
+
+    def get_time(self, name):
+        """
+        Gets the last time that the master with name, `name`, sent a hearbeat
+        message
+
+        Parameters
+        ----------
+        name: string
+            The name of the master
+
+        Returns
+        -------
+        A: float
+            The last time the master as sent a heartbeat message
+        """
+
+        return self.times[name]
+
+    def start(self):
+        """
+        Starts the name server thread
+
+        Notes
+        -----
+        If the name server is already running, a RuntimeWarning will be
+        thrown
+        """
+
         if self.running:
             raise RuntimeWarning("NameServer already running")
         else:
             def __thread():
                 while self.running:
-                    data_zip, addr = sock.recvfrom(1024)
+                    data_zip, _ = sock.recvfrom(1024)
                     data_str = zlib.decompress(data_zip)
                     data = json.loads(data_str)
                     self.master[data["name"]] = data
