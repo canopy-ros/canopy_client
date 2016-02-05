522b71de4aa004972848bef75abb32b5ed8a4af7
wallarelvo <alex@wallar.me>
2016-02-05 16:44:33 -0500

    Now publishing latency for debugging

--- src/server/ws.py
+++ src/server/ws.py
@@ -1,7 +1,10 @@
 
 # import zlib
 import json
+import rospy
+import time
 import common
+from std_msgs.msg import Float32
 from autobahn.twisted.websocket import WebSocketServerProtocol
 
 
@@ -11,11 +14,15 @@ class MMServerProtocol(WebSocketServerProtocol):
         name = request.path[1:]
         common.add_client(name, self)
         self.name_of_client = name
+        self.lat_pub = rospy.Publisher("/jammi/latency", Float32, queue_size=2)
 
     def onMessage(self, payload, is_binary):
         if not is_binary:
             try:
                 msg = json.loads(payload)
+                latency = Float32()
+                latency.data = time.time() - msg["stamp"]
+                self.lat_pub.publish(latency)
                 if msg["to"] == "*":
                     for name in common.clients.keys():
                         if name != msg["from"]:
